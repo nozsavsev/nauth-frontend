0e7ebbac2bf5e2aa7033ab7f290df592d80f560d
nozsavsev <nozsavsev@gmail.com>
2022-03-12 22:06:24 +0200

    initial

---
+++ pages/index.tsx
@@ -0,0 +1,247 @@
+const dev = process.env.NODE_ENV !== 'production'
+
+import cookie from 'react-cookies'
+import React, { useState, useEffect } from 'react'
+import { useRouter } from 'next/router';
+import axios from 'axios';
+
+
+export function Register({ onSuccess }: { onSuccess: any }) {
+
+    const [status, setStatus] = useState('');
+
+    const [username, setUsername] = useState('');
+    const [email, setEmail] = useState('');
+
+    const [password, setPassword] = useState('');
+    const [passwordOneMoreTime, setPasswordOneMoreTime] = useState('');
+
+    return (
+        <div style={{
+            margin: "20px",
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
+            alignItems: "center",
+            padding: "1em",
+            borderRadius: "0.5em",
+            boxShadow: "0px 0px 20px 1px #ccc"
+        }}>
+
+            <div style={{ minHeight: "1em", fontSize: "0.7em", maxWidth: "15em", wordBreak: "break-word", margin: "0.2em" }}>
+                {status}
+            </div>
+
+            <input className='flatInput' value={username} placeholder="username" onChange={(e) => { setUsername(e.target.value); }} />
+            <input className='flatInput' value={email} placeholder="email" onChange={(e) => { setEmail(e.target.value); }} />
+
+            <input className='flatInput' value={password} placeholder="password" type={"password"} onChange={(e) => { setPassword(e.target.value); }} />
+            <input className='flatInput' value={passwordOneMoreTime} placeholder="one more time" type={"password"} onChange={(e) => { setPasswordOneMoreTime(e.target.value); }} />
+
+            <button className='Button' onClick={() => {
+
+                if (password !== passwordOneMoreTime)
+                    return setStatus("Passwords don't match");
+
+                var payload = Buffer.from(JSON.stringify({ username: username, email: email, password: password })).toString('base64');;
+                axios.get(`${location.origin}/api/public/registerUserWithEmail?data=${payload}`).then(res => {
+
+                    if (res.data.code === "SUREUS")
+                        onSuccess(res.data)
+                    else
+                        setStatus(res.data.message);
+
+                }).catch(err => { console.log(err); });
+            }}>Register</button>
+        </div>
+    )
+}
+export function Login({ onSuccess }: { onSuccess: any }) {
+
+    const [status, setStatus] = useState('');
+
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
+    return (
+        <div style={{
+            margin: "20px",
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
+            alignItems: "center",
+            padding: "1em",
+            borderRadius: "0.5em",
+            boxShadow: "0px 0px 20px 1px #ccc"
+        }}>
+
+            <div style={{ minHeight: "1em", fontSize: "0.7em", maxWidth: "15em", wordBreak: "break-word", margin: "0.2em" }}>
+                {status}
+            </div>
+
+            <input className='flatInput' value={username} placeholder="username or email" onChange={(e) => { setUsername(e.target.value); }} />
+            <input className='flatInput' value={password} placeholder="password" type={"password"} onChange={(e) => { setPassword(e.target.value); }} />
+
+            <button className='Button' onClick={() => {
+                var payload = Buffer.from(JSON.stringify({ username: username, password: password })).toString('base64');;
+                axios.get(`${location.origin}/api/public/LoginWithPassword?data=${payload}`).then(res => {
+
+                    if (res.data.code === "SLOGIN")
+                        onSuccess(res.data)
+                    else
+                        setStatus(res.data.message);
+
+                }).catch(err => { console.log(err); });
+            }}>Login</button>
+        </div>
+    )
+}
+export function SessionManager({ onError }: { onError: any }) {
+
+    const [sessions, setSessions] = useState([]);
+
+    useEffect(() => {
+
+        var interval = setInterval(() => {
+
+            axios.get(`${location.origin}/api/private/getSessions`).then(res => {
+
+                if (res.data.code === "SESFET") {
+                    setSessions(res.data.sessions);
+                }
+                else {
+                    onError(res.data);
+                }
+            }).catch(err => { console.log(err); });
+
+
+
+        }, 1000)
+
+        return () => { clearInterval(interval) }
+
+    })
+
+
+    return (
+        <div style={{
+            margin: "20px",
+            width: "350px",
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "flex-start",
+            alignItems: "center",
+            padding: "1em",
+            borderRadius: "0.5em",
+            boxShadow: "0px 0px 20px 1px #ccc"
+        }}>
+
+            <table>
+                <tbody>
+                    {
+                        sessions.map((session, index) => {
+
+                            return <tr key={index}>
+
+                                <td>
+                                    <table style={{ fontWeight: session.current ? "bold" : "normal" }}>
+                                        <tbody>
+                                            <tr>
+                                                <td> {session.useragent.os} |</td>
+                                                <td> {session.useragent.browser} </td>
+                                            </tr>
+                                            <tr>
+                                                <td colSpan={3} style={{ fontSize: "0.5em", color: "#888" }}>{session.sessionID}</td>
+                                            </tr>
+                                        </tbody>
+                                    </table>
+                                </td>
+
+                                <td> <button className='Button' style={{ fontSize: "15px", }} onClick={() => {
+
+                                    var payload = Buffer.from(JSON.stringify({ sessionID: session.sessionID })).toString('base64');;
+
+                                    axios.get(`${location.origin}/api/private/revokeSession?data=${payload}`).then(res => {
+                                        console.log(res.data);
+                                    }).catch(err => { console.log(err); });
+
+                                }}>Logout</button></td>
+                            </tr>
+                        })
+                    }
+                </tbody>
+            </table>
+
+        </div>
+    )
+}
+
+
+
+export default function Page() {
+
+    const router = useRouter()
+    const [location, setLocation] = useState(null);
+    const [token, __setToken] = useState(cookie.load('token'));
+
+    const [authStatus, setAuthStatus] = useState(false);
+    const [user, setUser] = useState(false);
+
+    function setToken(token: string) {
+        __setToken(token);
+        cookie.save('token', token, { path: '/' });
+    }
+
+    useEffect(() => {
+        setLocation(window.location);
+
+        var params = (new URL(document.location.toString())).searchParams;
+        //params.get('darkMode')
+
+        if (token) {
+
+
+            var payload = Buffer.from(JSON.stringify({ token: token })).toString('base64');;
+
+            axios.get(`${window.location.origin}/api/public/AuthByToken?data=${payload}`).then(res => {
+               
+                console.log(res.data);
+
+                if (res.data.code === "TOKDEC") {
+                    setAuthStatus(true);
+                    setUser(res.data.user);
+                    setToken(res.data.newToken);
+                }
+
+            }).catch(err => { console.log(err); });
+
+        }
+
+
+    }, []);
+
+
+
+    if (authStatus === false)
+        return (
+            <div className="Container" style={{ flexDirection: "column", backgroundColor: "white" }}>
+
+                <Login onSuccess={(data) => { setToken(data.token); setAuthStatus(true); setUser(data.user); }} />
+
+            </div>
+        )
+    else
+        return (
+            <div className="Container" style={{ flexDirection: "column", backgroundColor: "white" }}>
+
+                <SessionManager onError={(data) => {
+                    setToken(null);
+                    setAuthStatus(false);
+                    setUser(null);
+                }} />
+
+            </div>
+        )
+
+
+}
--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 const port = 3000
 const dev = process.env.NODE_ENV !== 'production'
 
-
-const { uuid } = require('uuidv4');
+//misc
+const { uuid, regex } = require('uuidv4');
+const crypto = require('crypto')
 
 //nextJS server
 const nextjs = require('next')
@@ -12,128 +13,440 @@ const handle = next.getRequestHandler()
 //create express rest api server
 var cookieParser = require('cookie-parser')
 var bodyParser = require('body-parser')
-const express = require('express')
+var useragent = require('express-useragent');
 const cors = require('cors')
+
+const express = require('express')
 const app = express()
 const server = require('http').createServer(app)
 
-
 //init database
 var MongoClient = require('mongodb').MongoClient;
-const mongoURL = "mongodb://root:906abe19069e41fb93fe88f5d8060083@188.166.170.244:27017/?authSource=admin&readPreference=primary&appname=MongoDB%20Compass&ssl=false"
+const mongoURL = "mongodb://650a328cce5542068230388e45d12cd1:35120b4c0aba424c8c33d414241f8e80@207.154.197.15:27017/?authSource=admin&readPreference=primary&appname=MongoDB%20Compass&directConnection=true&ssl=false"
 const client = new MongoClient(mongoURL);
 
-//init firebase auth
-var admin = require("firebase-admin");
-var serviceAccount = require("./nozsa-com-firebase-adminsdk.json");
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount)
-});
-
-
-
-
-
-console.log(dev)
+const jwt = require('jsonwebtoken');
+const JWT_key = "ad47eef533a2e5271b64db04ce160939cef2099550cd7c119f835e75c4a2ed06"
 
 next.prepare().then(() => {
 
   app.use(cors())
   app.use(cookieParser())
   app.use(bodyParser())
+  app.use(useragent.express());
 
-  app.get('/api/getCookies', async function (req, res) {
-    res.json(req.cookies);
+  /*
+  codes
+      SMAINT - server is on active maintenance
+  */
+  app.use('/api', async function (req, res, next) {
+    //check maintenance
+    next();
   })
 
-
+  /*
+  codes
+      LEDATR - login failed due to incorrect data
+      AUTHER - auth error
+      ACCDEN - acess deny error
+  */
   app.use('/api/private', async function (req, res, next) {
 
-    var decoded = null;
-    try {
+    token = req.cookies.token;
 
-      await admin.auth()
-        .verifyIdToken(req.cookies.token).then(
-          (decodedToken) => {
-            decoded = decodedToken;
-          })
-        .catch((error) => {
-          decoded = null;
-        });
-    } catch { };
+    if (!token)
+      return res.json({ status: "error", message: "less data then required | request should contain token", code: "LEDATR" });
 
-    if (decoded == null)
-      return res.json({ error: "auth-required" });
+    var decodedToken = null;
+    try { decodedToken = jwt.verify(token, JWT_key); } catch (err) { }
 
-    if (decoded && !decoded.email_verified)
-      return res.json({ error: "email-verefication-required" });
-
+    if (!decodedToken)
+      return res.json({ status: "error", message: "invalid token", code: "AUTHER" });
 
     await client.connect();
-    const database = client.db("users");
+    const database = client.db("auth");
     const users = database.collection("users");
 
-    var user = await users.findOne({ uid: decoded?.uid });
 
-    if (user == null) {
-      await users.insertOne({ uid: decoded.uid, email: decoded.email, createdAt: new Date(), fullfilled: false, name: "", username: "", gender: "", role: "", testerGuiAllowed: false });
-      user = await users.findOne({ uid: decoded.uid });
+    var user = null;
+    user = await users.findOne({ id: decodedToken.data.userId });
+    if (!user) {
+      //await client.close();
+      return res.json({ status: "error", message: "invalid token no user found", code: "AUTHER" });
     }
 
-    client.close();
+    var session = user.sessionStorage.find(session => session.sessionID === decodedToken.data.sessionID);
+
+    if (!session) {
+      //await client.close();
+      return res.json({ status: "error", message: "invalid token no session found", code: "AUTHER" });
+    }
+
+    //check token hash
+    var decodedTokenHash = crypto.createHash(user.credentials.algorithm)
+      .update(user.credentials.salt)
+      .update(token)
+      .update(user.credentials.salt)
+      .digest('hex');
+
+    if (session.tokenHash !== decodedTokenHash)
+      return res.json({ status: "error", message: "invalid token hash missmatch", code: "AUTHER" });
+
+    //cleanup invalid sessions
+    user.sessionStorage.forEach((ses, si) => {
+      if (ses.expiryDate < Date.now() && ses.sessionID !== decodedToken.data.sessionID) {
+        user.sessionStorage.splice(si, 1);
+      }
+    });
+
+    //update user
+    await users.updateOne({ id: user.id }, { $set: { sessionStorage: user.sessionStorage } });
+    //await client.close();
 
     req.user = user;
+    req.session = session;
     next();
   })
 
 
-  app.get('/api/private/getUser', async function (req, res) {
-    res.json(req.user);
-  })
+  /*
+  codes
+      USALEX - user already exists
+      SUREUS - successfully registered user
+      LEDATR - invalid request less data then required
+      PADMAR - password doesn't match regex
+      USDMAR - username doesn't match regex
+      EMDMAR - email doesn't match regex
+  */
+  app.get('/api/public/registerUserWithEmail', async function (req, res) {
 
+    var passwordRegex = /^[A-z0-9*()$%@#]{8,32}$/gm;
+    var usernameRegex = /^[A-z0-9_]{3,32}$/gm;
+    var emailRegex = /[A-z0-9\.]{1,}\@[A-z0-9-\.]{1,}\.[A-z0-9-\.]{1,}/gm;
+    var data = JSON.parse(Buffer.from(req.query.data, 'base64').toString());
 
-  app.get('/api/private/getUsers', async function (req, res) {
+    //if request have all required data
+    if (!data.username || !data.email || !data.password)
+      return res.json({ status: "error", message: "less data then required | request should contain username", code: "LEDATR" });
+
+    //check password validity
+    if (data.password.length < 8 || data.password.length > 32 || passwordRegex.exec(data.password)?.length !== 1)
+      return res.json({ status: "error", message: "password doesn't match regex (8-32 symbols [A-z0-9*()$%@#])", code: "PADMAR" });
+
+    //check username validity
+    if (data.username.length < 3 || data.username.length > 32 || usernameRegex.exec(data.username)?.length !== 1)
+      return res.json({ status: "error", message: "username doesn't match regex (3-32 symbols [A-z0-9_])", code: "USDMAR" });
+
+    //check email validity
+    if (data.email.length < 3 || data.email.length > 32 || emailRegex.exec(data.email)?.length !== 1)
+      return res.json({ status: "error", message: "email doesn't match regex", code: "EMDMAR" });
+
 
     await client.connect();
-    const database = client.db("users");
+    const database = client.db("auth");
     const users = database.collection("users");
 
-    if (req.user.role === 'admin') {
-      var users_data = [];
-      await users.find().forEach((e) => { users_data.push(e) });
-      res.json(users_data);
+    //check if user with this username already exists
+    if (await users.findOne({ username: data.username }) !== null) {
+      //await client.close();
+      return res.json({ status: "error", message: "user with this username already exists", code: "USALEX" });
     }
-    else {
-      res.json({});
+    //check if user with this email already exists
+    if (await users.findOne({ email: data.email }) !== null) {
+      //await client.close();
+      return res.json({ status: "error", message: "user with this email already exists", code: "USALEX" });
     }
+
+    //generate credentials
+    var credentials = { passwordHash: "", salt: "", algorithm: "sha512" };
+    credentials.salt = uuid(); //salt
+    credentials.passwordHash = crypto.createHash(credentials.algorithm)
+      .update(credentials.salt)
+      .update(data.password)
+      .update(credentials.salt)
+      .digest('hex');          //hash
+
+    //create user object
+    var user = {
+      id: uuid(),
+      credentials: credentials,
+      username: data.username,
+      email: data.email,
+      providers: ["email"],
+      roles: ["user", "filled"],
+      sessionStorage: [],
+    }
+
+    //insert user
+    await users.insertOne(user);
+    //await client.close();
+
+    res.json({ status: "success", message: "successfully registered user", user: user, code: "SUREUS" });
   })
 
 
-  //auth api requests
-  app.get('/api/private/getUser', async (req, res) => {
-    res.json({ status: "success", user: req.user });
+  /*
+    codes
+        NOUSFO - no user found
+        SLOGIN - successfully logged in
+        LEDATR - invalid request less data then required
+        INPASS - incorrect password 
+        USDMAR - username doesn't match regex
+    */
+  app.get('/api/public/LoginWithPassword', async function (req, res) {
+
+    var passwordRegex = /^[A-z0-9*()$%@#]{8,32}$/gm;
+    var usernameRegex = /^[A-z0-9_@.]{3,32}$/gm;
+
+    var data = JSON.parse(Buffer.from(req.query.data, 'base64').toString());
+
+
+
+    //if request have all required data
+    if (!data.username || !data.password)
+      return res.json({ status: "error", message: "less data then required | request should contain username", code: "LEDATR" });
+
+    //check password validity
+    if (data.password.length < 8 || data.password.length > 32 || passwordRegex.exec(data.password)?.length !== 1)
+      return res.json({ status: "error", message: "password doesn't match regex (8-32 symbols [A-z0-9*()$%@#])", code: "PADMAR" });
+
+    //check username validity
+    if (data.username.length < 3 || data.username.length > 32 || usernameRegex.exec(data.username)?.length !== 1)
+      return res.json({ status: "error", message: "username doesn't match regex (3-32 symbols [A-z0-9_])", code: "USDMAR" });
+
+
+
+    await client.connect();
+    const database = client.db("auth");
+    const users = database.collection("users");
+
+    var user = null;
+
+    //determine what to use email or username
+    if (data.username.includes("@"))
+      user = await users.findOne({ email: data.username });
+    else
+      user = await users.findOne({ username: data.username });
+
+    //check if user exists
+    if (!user) {
+      //await client.close();
+      return res.json({ status: "error", message: "no user found", code: "NOUSFO" });
+    }
+
+
+
+    //generate credentials
+    var credentials = user.credentials;
+
+    //generate hash from incoming password
+    var incomingPasswordHash = crypto.createHash(credentials.algorithm)
+      .update(credentials.salt)
+      .update(data.password)
+      .update(credentials.salt)
+      .digest('hex');
+
+    //compare hashes
+    if (incomingPasswordHash !== credentials.passwordHash) {
+      //await client.close();
+      return res.json({ status: "error", message: "incorrect password", code: "INPASS" });
+    }
+
+
+    //generate token 
+
+    var sessionID = uuid();
+    var token = jwt.sign({ data: { userId: user.id, sessionID: sessionID }, }, JWT_key, { expiresIn: 60 * 60 * 24 * 365 });//1year expiry
+
+    var tokenHash = crypto.createHash(credentials.algorithm)
+      .update(credentials.salt)
+      .update(token)
+      .update(credentials.salt)
+      .digest('hex');
+
+    //generate session
+    var mongo_session = {
+      tokenHash: tokenHash,
+      sessionID: sessionID,
+      expiryDate: new Date(Date.now() + 1000 * 60 * 60 * 24 * 365),//2m expiry
+      useragent: { browser: req.useragent.browser, os: req.useragent.os, platform: req.useragent.platform, ip: req.socket.remoteAddress },
+    }
+
+    //add session to user
+    user.sessionStorage.push(mongo_session);
+
+    //update user
+    await users.updateOne({ id: user.id }, { $set: { sessionStorage: user.sessionStorage } });
+
+    //cleanup use before sending response
+    //cleanup user before sending response
+    user.sessionStorage = user.sessionStorage.map(ses => {
+      return {
+        current: ses.sessionID === mongo_session.sessionID,
+        sessionID: ses.sessionID,
+        expiryDate: ses.expiryDate,
+        useragent: ses.useragent,
+      };
+    });
+
+    user = {
+      username: user.username,
+      email: user.email,
+      sessionStorage: user.sessionStorage,
+      id: user.id,
+    }
+    //await client.close();
+
+    res.json({ status: "success", message: "successfully logged in", user: user, token: token, code: "SLOGIN" });
+
   })
 
+  /*
+    codes
+        INVTOK - invalid token
+        TOKDEC - decoded token
+  */
+  app.get('/api/public/AuthByToken', async function (req, res) {
 
-  app.post('/api/private/updateUser', async (req, res) => {
+    var data = JSON.parse(Buffer.from(req.query.data, 'base64').toString());
 
-    if (req.user.role === 'admin')
-      if (req.user.uid !== req.body.u.uid) {
+    if (!data.token)
+      return res.json({ status: "error", message: "less data then required | request should contain token", code: "LEDATR" });
 
-        await client.connect();
-        const database = client.db("users");
-        const users = database.collection("users");
+    await client.connect();
+    const database = client.db("auth");
+    const users = database.collection("users");
 
-        console.log(req.body.u.role, req.body.u.testerGuiAllowed);
 
-        await users.updateOne({ uid: req.body.u.uid }, { $set: { role: req.body.u.role } });
-        await users.updateOne({ uid: req.body.u.uid }, { $set: { testerGuiAllowed: req.body.u.testerGuiAllowed } });
-        client.close();
+    var decodedToken = null;
 
+    try { decodedToken = jwt.verify(data.token, JWT_key); } catch (err) { }
+
+    if (!decodedToken) {
+      //await client.close();
+      return res.json({ status: "error", message: "invalid token", code: "INVTOK" });
+    }
+    var user = null;
+
+    //findUserByID
+    user = await users.findOne({ id: decodedToken.data.userId });
+
+    if (!user) {
+      //await client.close();
+      return res.json({ status: "error", message: "invalid token no user found", code: "INVTOK" });
+    }
+    var session = user.sessionStorage.find(session => session.sessionID === decodedToken.data.sessionID);
+
+    if (!session) {
+      //await client.close();
+      return res.json({ status: "error", message: "invalid token no session found", code: "INVTOK" });
+    }
+    //check token hash
+    var decodedTokenHash = crypto.createHash(user.credentials.algorithm)
+      .update(user.credentials.salt)
+      .update(data.token)
+      .update(user.credentials.salt)
+      .digest('hex');
+
+    if (session.tokenHash !== decodedTokenHash) {
+      //await client.close();
+      return res.json({ status: "error", message: "invalid token hash missmatch", code: "INVTOK" });
+    }
+
+    //generate new token
+    var newToken = jwt.sign({ data: decodedToken.data, }, JWT_key, { expiresIn: 60 * 60 * 24 * 365 });//1 year expiry;
+
+    //cleanup invalid sessions
+    user.sessionStorage.forEach((ses, si) => {
+      if (ses.sessionID === decodedToken.data.sessionID) {
+
+        ses.tokenHash = crypto.createHash(user.credentials.algorithm)
+          .update(user.credentials.salt)
+          .update(newToken)
+          .update(user.credentials.salt)
+          .digest('hex');
+
+        ses.expiryDate = new Date(Date.now() + 1000 * 60 * 60 * 24 * 365);//1 year expiry
       }
+      else if (ses.expiryDate < Date.now()) {
+        user.sessionStorage.splice(si, 1);
+      }
+    });
+
+    //update user
+    await users.updateOne({ id: user.id }, { $set: { sessionStorage: user.sessionStorage } });
+
+    //cleanup user before sending response
+    user.sessionStorage = user.sessionStorage.map(ses => {
+      return {
+        current: ses.sessionID === decodedToken.data.sessionID,
+        sessionID: ses.sessionID,
+        expiryDate: ses.expiryDate,
+        useragent: ses.useragent,
+      };
+    });
+
+    user = {
+      username: user.username,
+      email: user.email,
+      sessionStorage: user.sessionStorage,
+      id: user.id,
+    }
+
+    //await client.close();
+
+    res.json({ status: "success", message: "decoded token", user: user, newToken: newToken, code: "TOKDEC" });
+  })
 
 
-    res.json({});
+  /*
+    codes
+        INVSID - invalid session
+        SESREV - succesfully revoked session
+  */
+  app.get('/api/private/revokeSession', async function (req, res) {
+
+    var data = JSON.parse(Buffer.from(req.query.data, 'base64').toString());
+
+    if (!data.sessionID)
+      return res.json({ status: "error", message: "less data then required | request should contain sessionID", code: "LEDATR" });
+
+    await client.connect();
+    const database = client.db("auth");
+    const users = database.collection("users");
+
+    var user = req.user;
+    user.sessionStorage.forEach((ses, si) => {
+      if (ses.sessionID === data.sessionID) {
+        user.sessionStorage.splice(si, 1);
+      }
+    })
+
+    //update user
+    await users.updateOne({ id: user.id }, { $set: { sessionStorage: user.sessionStorage } });
+    //await client.close();
+
+    res.json({ status: "success", message: "revoked session", code: "SESREV" });
+  })
+
+
+
+  /*
+    codes
+        SESFET - succesfully fetched session
+  */
+  app.get('/api/private/getSessions', async function (req, res) {
+
+    req.user.sessionStorage = req.user.sessionStorage.map(ses => {
+      return {
+        current: ses.sessionID === req.session.sessionID,
+        sessionID: ses.sessionID,
+        expiryDate: ses.expiryDate,
+        useragent: ses.useragent,
+      };
+    });
+
+    res.json({ status: "success", sessions: req.user.sessionStorage, code: "SESFET" });
   })
 
 
@@ -144,6 +457,6 @@ next.prepare().then(() => {
   })
 
   server.listen(port, () => {
-    console.log(`click! => Ready and listen on ${port}`)
+    console.log(`nauth! => Ready and listen on ${port}`)
   })
 });
